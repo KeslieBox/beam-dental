9a61b06b8e5f49d8451a908dd024e888c5f1be5e
KeslieBox <kesliebox@gmail.com>
2021-08-19 13:49:38 -0500

    add testing needs to App.test.js

--- beam-dental/src/App.test.js
+++ beam-dental/src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders hello', () => {
+//   render(<App />);
+//   const text = screen.getByText(/hello/);
+//   expect(text).toBeInTheDocument();
+// });
+
+// Tests to write:
+  // upon entering shipping screen starting box summery displayed
+
+  // shipping screen has two tabs on it: 
+    // one for generating and displaying starter boxes
+    // one for generating and displaying refill boxes
+  
+  // user can toggle between starter and refill boxes
+
+  // if endpoint fails, display error message: 'Error: Could not receive family preferences.'
+
+  // boxes should be grouped by matching color, ie 2 blue brushes grouped together with blue replacement head
+    // they should not be grouped w/ other colors
+
+  // replacement heads can be grouped 4 to a box and should be grouped by color
+
+
+
+
